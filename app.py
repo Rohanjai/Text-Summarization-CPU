8f834767aed42fba7b0ff9249ad7845ef7e8eb31
Rohanjai <jairohan2912@gmail.com>
2023-09-20 21:26:22 +0530

    Function to display pdf added

--- app.py
+++ app.py
@@ -36,3 +36,19 @@ def llm_pipeline(filepath):
     result = pipe_sum(input_text)
     result = result[0]['summary_text']
     return result
+
+@st.cache_data
+#function to display the PDF of a given file 
+def displayPDF(file):
+    # Opening file from file path
+    with open(file, "rb") as f:
+        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+
+    # Embedding PDF in HTML
+    pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="100%" height="600" type="application/pdf"></iframe>'
+
+    # Displaying File
+    st.markdown(pdf_display, unsafe_allow_html=True)
+
+#streamlit code 
+st.set_page_config(layout="wide")
