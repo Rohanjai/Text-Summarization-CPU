64542f9f14a55502df7e2de29135172a185b4156
Rohanjai <jairohan2912@gmail.com>
2023-09-20 21:25:17 +0530

    Model Loaded and Preprocessing done

---
+++ app.py
@@ -0,0 +1,25 @@
+import streamlit as st 
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain.chains.summarize import load_summarize_chain
+from transformers import T5Tokenizer, T5ForConditionalGeneration
+from transformers import pipeline
+import torch
+import base64
+
+#model and tokenizer loading
+checkpoint = "LaMini-Flan-T5-248M"
+tokenizer = T5Tokenizer.from_pretrained(checkpoint)
+base_model = T5ForConditionalGeneration.from_pretrained(checkpoint, device_map='auto', torch_dtype=torch.float32)
+
+#file loader and preprocessing
+def file_preprocessing(file):
+    loader =  PyPDFLoader(file)
+    pages = loader.load_and_split()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=200, chunk_overlap=50)
+    texts = text_splitter.split_documents(pages)
+    final_texts = ""
+    for text in texts:
+        print(text)
+        final_texts = final_texts + text.page_content
+    return final_texts
